7125b575a3f5699bec1a988962a79ecf06f0c0f2
Irina Saushkina <irinasauskina02@gmail.com>
2021-02-14 16:42:39 +0400

    Дописла анимацию на появление окна и js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const modalButton = document.querySelectorAll('.modal-button-item p'),
-    modalItem = document.querySelectorAll('.modal-item');    
-modalButton.forEach((item, i) => {
-    item.addEventListener('click', (e) => {
-       
+    modalItem = document.querySelectorAll('.modal-item'),
+    buttonClose = document.querySelectorAll('.close-modal'),
+    backgroundModal = document.querySelector('.backgraund-modal');
+    
+function closeModal(index) {    
+        modalItem[index].classList.remove('active');
+        backgroundModal.classList.remove('active');    
+}        
+modalButton.forEach(item => {
+    item.addEventListener('click', () => {       
         const num = item.getAttribute('data-num');
         modalItem[num].classList.add('active');
-       
+        backgroundModal.classList.add('active');
+        document.body.addEventListener('click', (e) => {            
+            if(e.target == backgroundModal) {
+                closeModal(num);
+            }
+        }); 
+    });    
+});
+buttonClose.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        closeModal(i);
     });
-    
-
-   
 });
